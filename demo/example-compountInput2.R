29e1bf72a069bb4dbb87724966ac916ecfb0dc1d
dipterix <dipterix.wang@gmail.com>
2019-10-13 15:35:30 -0500

    make demo local

--- demo/example-compountInput2.R
+++ demo/example-compountInput2.R
@@ -1,48 +1,50 @@
 library(shiny)
 library(dipsaus)
-ui <- fluidPage(
-  fluidRow(
-    column(
-      width = 4,
-      compoundInput2(
-        'compound', 'Group Label', label_color = 1:10,
-        components = div(
-          textInput('txt', 'Text'),
-          selectInput('sel', 'Select', choices = 1:10, multiple = TRUE),
-          sliderInput('sli', 'Slider', max=1, min=0, val=0.5)
+local({
+  ui <- fluidPage(
+    fluidRow(
+      column(
+        width = 4,
+        compoundInput2(
+          'compound', 'Group Label', label_color = 1:10,
+          components = div(
+            textInput('txt', 'Text'),
+            selectInput('sel', 'Select', choices = 1:10, multiple = TRUE),
+            sliderInput('sli', 'Slider', max=1, min=0, val=0.5)
+          ),
+          value = list(
+            list(txt = '1'),  # Set text first group to be "1"
+            '',                # no settings for second group
+            list(sli = 0.2)    # sli = 0.2 for the third group
+          )
         ),
-        value = list(
-          list(txt = '1'),  # Set text first group to be "1"
-          '',                # no settings for second group
-          list(sli = 0.2)    # sli = 0.2 for the third group
-        )
-      ),
-      hr(),
-      actionButton('action', 'Update compound input')
+        hr(),
+        actionButton('action', 'Update compound input')
+      )
     )
   )
-)
 
-server <- function(input, output, session) {
-  observe({
-    print(input$compound)
-  })
-  observe({
-    # Getting specific input at group 1
-    print(input$compound_txt_1)
-  })
-  observeEvent(input$action, {
-    updateCompoundInput2(
-      session, 'compound',
-      # Update values for each components
-      value = lapply(1:5, function(ii){
-        list(
-          txt = sample(LETTERS, 1),
-          sel = sample(1:10, 3),
-          sli = runif(1)
-        )
-      }), ncomp = NULL, txt = list(label = as.character(Sys.time())))
-  })
-}
+  server <- function(input, output, session) {
+    observe({
+      print(input$compound)
+    })
+    observe({
+      # Getting specific input at group 1
+      print(input$compound_txt_1)
+    })
+    observeEvent(input$action, {
+      updateCompoundInput2(
+        session, 'compound',
+        # Update values for each components
+        value = lapply(1:5, function(ii){
+          list(
+            txt = sample(LETTERS, 1),
+            sel = sample(1:10, 3),
+            sli = runif(1)
+          )
+        }), ncomp = NULL, txt = list(label = as.character(Sys.time())))
+    })
+  }
 
-shinyApp(ui, server, options = list(launch.browser = TRUE))
+  shinyApp(ui, server, options = list(launch.browser = TRUE))
+})
