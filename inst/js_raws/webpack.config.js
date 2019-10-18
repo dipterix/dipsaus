32929e78914aa1f080b76f8536ad86e5c438de0f
dipterix <dipterix.wang@gmail.com>
2019-10-18 15:42:42 -0500

    Initialize js lib

---
+++ inst/js_raws/webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require("webpack");
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, '../shiny-addons/dipsaus/'),
+    filename: "dipsaus-dipterix-lib.js"
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new UglifyJsPlugin({
+      include: /\.min\.js$/
+    })]
+  }
+};
