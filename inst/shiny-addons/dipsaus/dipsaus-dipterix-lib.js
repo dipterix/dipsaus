602da79cead77cdf1a68f9ea27da8ebdbb461d2c
dipterix <dipterix.wang@gmail.com>
2020-03-19 00:16:27 -0500

    updated npm dep

--- inst/shiny-addons/dipsaus/dipsaus-dipterix-lib.js
+++ inst/shiny-addons/dipsaus/dipsaus-dipterix-lib.js
@@ -7128,6 +7128,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+// ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
 
 // CONCATENATED MODULE: ./src/utils.js
