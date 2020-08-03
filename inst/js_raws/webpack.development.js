13e0d9f6d3f33caef4a7b3090fb698c35a1e7173
dipterix <zw23@rice.edu>
2020-08-02 23:49:08 -0500

    uglifyjs (deprecated) to terser

--- inst/js_raws/webpack.development.js
+++ inst/js_raws/webpack.development.js
@@ -1,6 +1,6 @@
 const webpack = require("webpack");
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'development',
