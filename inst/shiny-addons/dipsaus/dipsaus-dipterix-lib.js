b33e257daf6ac4112bfa75d074bdb22aae44aa15
dipterix <zw23@rice.edu>
2021-07-06 21:21:24 -0400

    update js libs

--- inst/shiny-addons/dipsaus/dipsaus-dipterix-lib.js
+++ inst/shiny-addons/dipsaus/dipsaus-dipterix-lib.js
@@ -1,92 +1,8 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 120:
+/***/ (function(module) {
 
 /*!
  * html2canvas 1.0.0-rc.5 <https://html2canvas.hertzen.com>
@@ -95,7 +11,7 @@
  */
 (function (global, factory) {
      true ? module.exports = factory() :
-    undefined;
+    0;
 }(this, function () { 'use strict';
 
     /*! *****************************************************************************
@@ -7123,15 +7039,71 @@
 //# sourceMappingURL=html2canvas.js.map
 
 
-/***/ }),
-/* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ })
 
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
 "use strict";
-// ESM COMPAT FLAG
-__webpack_require__.r(__webpack_exports__);
 
-// CONCATENATED MODULE: ./src/utils.js
+;// CONCATENATED MODULE: ./src/utils.js
 
 
 function has_jquery() {
@@ -7218,10 +7190,10 @@ const document_ready = function(callback, context) {
 
 
 
-// CONCATENATED MODULE: ./src/shiny-input-compound.js
+;// CONCATENATED MODULE: ./src/shiny-input-compound.js
 
 
-class shiny_input_compound_CompountInputItem{
+class CompountInputItem{
 
   constructor( el, binding, Shiny ){
     this._id = el.id;
@@ -7531,7 +7503,7 @@ function register_compoundInput2 ( Shiny, debug = false ){
     if( has_jquery() ){
       _el = $(_el)[0];
     }
-    els[ _el.id ] = new shiny_input_compound_CompountInputItem( _el, binding, Shiny );
+    els[ _el.id ] = new CompountInputItem( _el, binding, Shiny );
   };
 
   binding.getValue = (el) => {
@@ -7588,7 +7560,7 @@ function register_compoundInput2 ( Shiny, debug = false ){
 
 
 
-// CONCATENATED MODULE: ./src/shiny-input-actionbutton2.js
+;// CONCATENATED MODULE: ./src/shiny-input-actionbutton2.js
 
 
 function register_actionButtonStyled ( Shiny, debug = false ){
@@ -7691,7 +7663,7 @@ $(document).on('click', 'a.action-button', function(e) {
   e.preventDefault();
 });
 */
-// CONCATENATED MODULE: ./src/shiny-input-arbitrary.js
+;// CONCATENATED MODULE: ./src/shiny-input-arbitrary.js
 
 
 const register_set_input = (Shiny, debug = false) => {
@@ -7734,10 +7706,9 @@ const register_set_input = (Shiny, debug = false) => {
 
 
 // EXTERNAL MODULE: ./node_modules/html2canvas/dist/html2canvas.js
-var html2canvas = __webpack_require__(0);
+var html2canvas = __webpack_require__(120);
 var html2canvas_default = /*#__PURE__*/__webpack_require__.n(html2canvas);
-
-// CONCATENATED MODULE: ./src/screenshot.js
+;// CONCATENATED MODULE: ./src/screenshot.js
 
 
 
@@ -7770,23 +7741,24 @@ const register_screenshot = (Shiny, debug = false) => {
 
 
 
-// CONCATENATED MODULE: ./src/index.js
+;// CONCATENATED MODULE: ./src/index.js
 
 
 
 
 
 
-const src_Shiny = window.Shiny;
+const Shiny = window.Shiny;
 const DEBUG = true;
 
-register_actionButtonStyled( src_Shiny, DEBUG );
-register_compoundInput2( src_Shiny, DEBUG );
-register_screenshot( src_Shiny, DEBUG );
-register_set_input( src_Shiny, DEBUG );
+register_actionButtonStyled( Shiny, DEBUG );
+register_compoundInput2( Shiny, DEBUG );
+register_screenshot( Shiny, DEBUG );
+register_set_input( Shiny, DEBUG );
 
 
+})();
 
-/***/ })
-/******/ ]);
+/******/ })()
+;
 //# sourceMappingURL=dipsaus-dipterix-lib.js.map
