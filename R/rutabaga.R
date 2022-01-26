80a592d997b92e5c7b9a3fb45b0154eeaf56e95d
z94007 <zhangxiangz94007@gmail.com>
2022-01-26 18:48:34 -0500

    can change na to 0 in mean_se

--- R/rutabaga.R
+++ R/rutabaga.R
@@ -4,6 +4,7 @@
 #' by default
 #' @param x R object
 #' @param na.rm whether to remove \code{NA}; default is false
+#' @param na_as_zero whether convert \code{NA} to zero
 #' @param ... passed to other methods
 #' @seealso \code{\link{mean_se}}
 #' @return A numerical number that is the standard error of the mean
@@ -17,24 +18,29 @@
 #' identical(ste_mean(x), sd(x) / sqrt(100))
 #'
 #' @export
-ste_mean <- function(x, na.rm = FALSE, ...) {
+ste_mean <- function(x, na.rm = FALSE, na_as_zero = na.rm, ...) {
   UseMethod("ste_mean")
 }
 
 #' @rdname ste_mean
 #' @export
-ste_mean.default <- function(x, na.rm = FALSE, ...) {
+ste_mean.default <- function(x, na.rm = FALSE, na_as_zero = na.rm,  ...) {
   n <- sum(!is.na(x))
   if(n < 2){
-    return(NA)
+    re <- NA
   }
-  stats::sd(x, na.rm = na.rm) / sqrt(n)
+  re <- stats::sd(x, na.rm = na.rm) / sqrt(n)
+  if(na_as_zero){
+    re <- 0
+  }
+  re
 }
 
 #' Calculates mean and standard error of mean
 #' @seealso \code{\link{ste_mean}}
 #' @param x R numerical object
 #' @param na.rm whether to remove \code{NA}; default is false
+#' @param se_na_as_zero see \code{na_as_zero} in \code{\link{ste_mean}}
 #' @return A named vector containing the \code{\link{mean}} and standard error
 #' of mean (\code{\link{ste_mean}}).
 #' @examples
@@ -44,7 +50,7 @@ ste_mean.default <- function(x, na.rm = FALSE, ...) {
 #' mean_se(rnorm(10000))
 #'
 #' @export
-mean_se <- function(x, na.rm = FALSE) {
-  c(mean = mean(x, na.rm = na.rm), se = ste_mean(x, na.rm = na.rm))
+mean_se <- function(x, na.rm = FALSE, se_na_as_zero = na.rm) {
+  c(mean = mean(x, na.rm = na.rm), se = ste_mean(x, na.rm = na.rm, na_as_zero = se_na_as_zero))
 }
 
