2b50c4175b3b199318fe30d6814621277812129e
dipterix <dipterix.wang@gmail.com>
2023-01-27 11:43:51 -0500

    added more shortcuts

--- shortcuts.R
+++ shortcuts.R
@@ -8,7 +8,12 @@
 # DIPSAUS DEBUG START
 # message("This is a debug message")
 dipsaus::rs_add_shortcut(1, {
-  dipsaus::rs_quick_debug()
+  local({
+    tik <- Sys.time()
+    dipsaus::rs_quick_debug()
+    tok <- Sys.time()
+    message("Exec time: ", format(tok - tik))
+  })
 })
 
 #' Save current workspace to a temporary place for quick debugging when
@@ -35,4 +40,49 @@ dipsaus::rs_add_shortcut(5, {
     }
   })
 })
+
+#' Insert operator `%<-?%`. This operator requires `dipsaus` to be loaded.
+#' It assigns the right-hand data to the left-hand variable if the
+#' right-hand variable is valid and not null. For example
+#'
+#' # assigns `a` with value `1`
+#' a %<-?% 1
+#'
+#' # does NOT assign `a` (do nothing)
+#' a %<-?% NULL
+#'
+#' You can remember the expression `a %?<-% b` as
+#'   "`b` is null or undefined? assign to `a` if yes, otherwise nothing"
+#'
+dipsaus::rs_add_shortcut(8, {
+  library(dipsaus)
+  local({
+    cts <- rstudioapi::getActiveDocumentContext()
+    rstudioapi::selectionSet(" %<-?% ", cts$id)
+    invisible(NULL)
+  })
+})
+
+
+#' Insert operator `%?<-%`. This operator requires `dipsaus` to be loaded.
+#' It assigns the right-hand data to the left-hand variable if the
+#' left-hand variable does not exist or is `NULL`. For example
+#'
+#' a %?<-% 1
+#'
+#' assigns `a` with value `1` if `a` is undefined, or is NULL, otherwise
+#' nothing will be done, and right-hand expression will not execute.
+#'
+#' You can remember the expression `a %?<-% b` as
+#'   "`a` is null or undefined? assign `a` if yes, otherwise nothing"
+#'
+dipsaus::rs_add_shortcut(9, {
+  library(dipsaus)
+  local({
+    cts <- rstudioapi::getActiveDocumentContext()
+    rstudioapi::selectionSet(" %?<-% ", cts$id)
+    invisible(NULL)
+  })
+})
+
 
