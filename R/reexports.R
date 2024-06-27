26a02b29140041e81c39bae07981ec3ffcab6e3e
dipterix <dipterix.wang@gmail.com>
2024-06-26 22:53:31 -0400

    Re-exportd jsonlite functions

--- R/reexports.R
+++ R/reexports.R
@@ -6,11 +6,11 @@ digest::digest
 #' @param object,... passed to \code{\link[digest]{digest}}
 #' @param keep_source whether to keep the code that generates the object;
 #' default is false
-#' @seealso \code{\link{remove_source}}
+#' @seealso \code{\link[utils]{removeSource}}
 #' @export
 digest2 <- function(object, ..., keep_source = FALSE) {
   if(!keep_source) {
-    remove_source(object)
+    object <- utils::removeSource(object)
   }
   digest::digest(object, ...)
 }
@@ -47,3 +47,29 @@ rlang::error_cnd
 #' @importFrom rlang trace_back
 #' @export
 rlang::trace_back
+
+
+#' @importFrom jsonlite read_json
+#' @export
+jsonlite::read_json
+
+#' @importFrom jsonlite write_json
+#' @export
+jsonlite::write_json
+
+#' @importFrom jsonlite serializeJSON
+#' @export
+jsonlite::serializeJSON
+
+#' @importFrom jsonlite unserializeJSON
+#' @export
+jsonlite::unserializeJSON
+
+#' @importFrom jsonlite fromJSON
+#' @export
+jsonlite::fromJSON
+
+#' @importFrom jsonlite toJSON
+#' @export
+jsonlite::toJSON
+
